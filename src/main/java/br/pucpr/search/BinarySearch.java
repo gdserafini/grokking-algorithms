7dbc610690e6b03a0bd39efe28c786d3700db6c0
gdserafini <gdserafini@outlook.com>
2025-05-03 11:01:48 -0300

    update -> binary search comments

--- src/main/java/br/pucpr/search/BinarySearch.java
+++ src/main/java/br/pucpr/search/BinarySearch.java
@@ -1,23 +1,42 @@
 package br.pucpr.search;
 
+/**
+ * Binary search return the position of a target value in a sorted list
+ * The running time is O(log n) -> 2^x = n
+ * (x is number of operations, so, time = optime x n)
+ */
 public class BinarySearch {
     public static <T extends Comparable<T>> int search(T[] values, T target){
-        if(values.length == 0 || values == null || target == null) {
-            return -1;
-        }
+        if(values.length == 0 || values == null || target == null) return -1;
 
         int start = 0;
         int end = values.length - 1;
-
+        
+        //end of iterations -> start == end (final guess)
         while(start <= end){
             int mid = (start + end) / 2;
-            int compare = values[mid].compareTo(target);
+            /**
+             * Returns a negative value if values[mid] is less than target
+             * Returns 0 if equal
+             * Return a positive value if greater
+             */
+            int comparison = values[mid].compareTo(target);
 
-            if(compare == 0) return mid;
-            if(compare < 0) start = mid + 1;
-            if(compare > 0) end = mid - 1;
+            if(comparison == 0) return mid; //value[mid] is equal to target
+            /**
+             * values[mid] is less than tagert
+             * So, the binary search will use the upper half of the list:
+             * start = next position from mid (mid + 1)
+             */
+            if(comparison < 0) start = mid + 1;
+            /**
+             * values[mid] is greater than target
+             * So, the BS will use the lower half of the list:
+             * end = previous position from the mid (mid - 1)
+             */
+            if(comparison > 0) end = mid - 1;
         }
         
-        return -1;
+        return -1; //not find
     }
 }
--- src/test/java/br/pucpr/search/BinarySearchTest.java
+++ src/test/java/br/pucpr/search/BinarySearchTest.java
@@ -5,27 +5,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BinarySearchTest {
     private Integer[] numbers = {1,2,3,4,5,6,7,8,9};
-    private int TARGET_MID = 5;
-    private int TARGET_LOW = 3;
-    private int TARGET_HIGH = 7;
 
     @Test
     public void testBinarySearchFindMid(){
-        assertEquals(BinarySearch.search(numbers, TARGET_MID), 4);
+        assertEquals(BinarySearch.search(numbers, 5), 4);
     }
 
     @Test
     public void testBinarySearchFindLow(){
-        assertEquals(BinarySearch.search(numbers, TARGET_LOW), 2);
+        assertEquals(BinarySearch.search(numbers, 3), 2);
     }
 
     @Test
     public void testBinarySearchFindHigh(){
-        assertEquals(BinarySearch.search(numbers, TARGET_HIGH), 6);
+        assertEquals(BinarySearch.search(numbers, 7), 6);
     }
 
     @Test
     public void testBinarySearchNotFind(){
         assertEquals(BinarySearch.search(numbers, 10), -1);
+    }
+
+    @Test
+    public void testBinarySearchStartOfList(){
+        assertEquals(BinarySearch.search(numbers, 9), 8);
+    }
+
+    @Test
+    public void testBinarySearchEndOfList(){
+        assertEquals(BinarySearch.search(numbers, 1), 0);
     }
 }
