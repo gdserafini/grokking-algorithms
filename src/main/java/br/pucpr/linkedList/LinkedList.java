d96fd6d6c98ece179c36a31a9be5b5941225606b
Giordano D. Serafini <gdserafini@outlook.com>
2025-05-21 15:35:39 -0300

    add -> add method with index

--- src/main/java/br/pucpr/linkedList/LinkedList.java
+++ src/main/java/br/pucpr/linkedList/LinkedList.java
@@ -1,36 +1,73 @@
 package br.pucpr.linkedList;
 
 public class LinkedList<T> {
-    private DataNode<T> top;
-    private DataNode<T> botton;
+    private DataNode<T> tail;
+    private DataNode<T> head;
     private int size;
 
     public LinkedList(){
-        this.top = null;
-        this.botton = null;
+        this.tail = null;
+        this.head = null;
         this.size = 0;
     }
 
     public void add(T data){
         var newNode = new DataNode<T>(data);
 
-        if(top == null && botton == null){
-            this.top = newNode;
-            this.botton = newNode;
+        if(this.size == 0){
+            this.tail = newNode;
+            this.head = newNode;
             this.size++;
             return;
         }
 
-        this.top.setNext(newNode);
-        newNode.setPrevious(top);
-        this.top = newNode;
+        this.tail.setNext(newNode);
+        newNode.setPrevious(this.tail);
+        this.tail = newNode;
+        this.size++;
+    }
+
+    public void add(T data, int index){
+        if(index > this.size || index < 0) return;
+
+        var newNode = new DataNode<T>(data);
+        
+        if(this.size == 0) this.add(data);
+
+        if(index == 0){
+            this.head.setPrevious(newNode);
+            newNode.setNext(this.head);
+            this.head = newNode;
+            this.size++;
+            return;
+        }
+        if(index == this.size){
+            this.tail.setNext(newNode);
+            newNode.setPrevious(this.tail);
+            this.tail = newNode;
+            this.size++;
+            return;
+        }
+
+        DataNode<T> currentNode = this.head;
+        
+        for(int i = 0; i < index; i++){
+            currentNode = currentNode.getNext();
+        }
+
+        DataNode<T> previous = currentNode.getPrevious();
+        
+        previous.setNext(newNode);
+        currentNode.setPrevious(newNode);
+        newNode.setPrevious(previous);
+        newNode.setNext(currentNode);
         this.size++;
     }
 
     public void print(){
-        DataNode<T> currentNode = this.botton;
+        DataNode<T> currentNode = this.head;
 
-        for(int i = 0; i <= size; i++){
+        while(currentNode != null){
             System.out.print(currentNode.getData() + " ");
             currentNode = currentNode.getNext();
         }
