e7ddc02e72f33dd34a2e815d6f516bcfb448a2de
Giordano D. Serafini <gdserafini@outlook.com>
2025-05-28 13:04:21 -0300

    add -> factorial calculation tests

--- src/test/java/br/pucpr/recursion/FactorialTest.java
+++ src/test/java/br/pucpr/recursion/FactorialTest.java
@@ -1,1 +1,28 @@
+package br.pucpr.recursion;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FactorialTest {
+
+    @Test
+    public void testFactorialOfZero() {
+        Factorial factorial = new Factorial();
+        assertEquals(1, factorial.calc(0), "0! should be 1");
+    }
+
+    @Test
+    public void testFactorialOfPositiveInteger() {
+        Factorial factorial = new Factorial();
+        assertEquals(120, factorial.calc(5), "5! should be 120");
+    }
+
+    @Test
+    public void testFactorialOfNegativeIntegerThrowsException() {
+        Factorial factorial = new Factorial();
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            factorial.calc(-3);
+        });
+        assertEquals("Invalid value: -3", exception.getMessage());
+    }
+}
