134922117e7cf029a8b3c5323dca83526d6268b2
Giordano D. Serafini <gdserafini@outlook.com>
2025-05-28 13:02:32 -0300

    add -> factorial calculation method

--- src/main/java/br/pucpr/recursion/Factorial.java
+++ src/main/java/br/pucpr/recursion/Factorial.java
@@ -1,1 +1,11 @@
+package br.pucpr.recursion;
 
+public class Factorial {
+
+    public int calc(int value){
+        if(value < 0) throw new IllegalArgumentException("Invalid value: " + value);
+        if(value == 0) return 1;
+        return value * calc(value - 1);
+    }
+
+}
