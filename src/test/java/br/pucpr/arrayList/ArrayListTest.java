e59a2409ccd152f757927010a3d074e7f69e61f2
gdserafini <gdserafini@outlook.com>
2025-05-29 19:33:46 -0300

    add -> array list test class

---
+++ src/test/java/br/pucpr/arrayList/ArrayListTest.java
@@ -0,0 +1,11 @@
+package br.pucpr.arrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class ArrayListTest {
+
+    @Test
+    public void test(){}
+
+}
