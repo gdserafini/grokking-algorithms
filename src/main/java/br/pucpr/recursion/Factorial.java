6c73b1161cc545062fa580ff9278ab26317d75c5
Giordano D. Serafini <gdserafini@outlook.com>
2025-05-29 14:00:58 -0300

    add -> tail recursion for factorial

--- src/main/java/br/pucpr/recursion/Factorial.java
+++ src/main/java/br/pucpr/recursion/Factorial.java
@@ -3,9 +3,26 @@ package br.pucpr.recursion;
 public class Factorial {
 
     public int calc(int value){
-        if(value < 0) throw new IllegalArgumentException("Invalid value: " + value);
-        if(value == 0) return 1;
+        validateValue(value);
+        if(value == 0 || value == 1) return 1;
         return value * calc(value - 1);
     }
 
+    private int calcTailHelper(int value, long acc){
+        if(value == 0 || value == 1) return acc;
+        return calcTail(value - 1; value * acc);
+    }
+
+    /*
+     *Note: The JVM do not optmize for tail recurssion
+     *Java still uses the call stack
+    */
+    public int calcTail(int value){
+        validateValue(value);
+        return calcTailHelper(value, 1);
+    }
+
+    private void validateValue(int value){
+        if(value < 0) throw new IllegalArgumentException("Invalid value: " + value);
+    }
 }
