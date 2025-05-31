2835adab1e197618f832c0994490998bed2ede76
gdserafini <gdserafini@outlook.com>
2025-05-31 12:17:28 -0300

    add -> euclid code and test files

---
+++ src/main/java/br/pucpr/euclid/Euclid.java
@@ -0,0 +1,15 @@
+package br.pucpr.euclid;
+
+import org.javatuples.Pair;
+
+public class Euclid {
+
+    public Pair<Integer, Integer> getMaxSides(int height, int width){
+        return null;
+    }
+
+    public int getGCD(int value1, int value2){
+        return 0;
+    }
+
+}
---
+++ src/test/java/br/pucpr/euclid/EuclidTest.java
@@ -0,0 +1,11 @@
+package br.pucpr.euclid;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class EuclidTest {
+
+    @Test
+    public void test(){}
+
+}
