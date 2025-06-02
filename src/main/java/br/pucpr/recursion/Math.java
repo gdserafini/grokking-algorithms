e633976faed050f4ac62baf3365ff9cec4bf1573
Giordano D. Serafini <gdserafini@outlook.com>
2025-06-02 15:40:57 -0300

    Update and rename Sum.java to Math.java

--- src/main/java/br/pucpr/recursion/Math.java
+++ src/main/java/br/pucpr/recursion/Math.java
@@ -1,8 +1,8 @@
 package br.pucpr.recursion;
 
-public class Sum {
+public class Math {
 
-    public int calc(int[] values, int start, int end){
+    public int sum(int[] values, int start, int end){
         if(end - start == 0) return values[start];
         return values[start] + calc(values, start + 1, end);
     }
