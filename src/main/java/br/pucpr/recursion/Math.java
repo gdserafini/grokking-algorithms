3010285032733416618fcf174ac33c468801b677
Giordano D. Serafini <gdserafini@outlook.com>
2025-06-02 15:57:34 -0300

    Update Math.java

--- src/main/java/br/pucpr/recursion/Math.java
+++ src/main/java/br/pucpr/recursion/Math.java
@@ -3,8 +3,32 @@ package br.pucpr.recursion;
 public class Math {
 
     public int sum(int[] values, int start, int end){
+        validateArrayIndexes(start, end);
         if(end - start == 0) return values[start];
         return values[start] + calc(values, start + 1, end);
+    }
+
+    public int size(int[] values, int start, int end){
+        validateArrayIndexes(start, end);
+        if(end - start == 0) return 1;
+        return values[start] + size(values, start + 1, end);
+    }
+
+    public int max(int[] values, int index, int max){
+        if(index < 0 || index >= values.length){
+            throw new IllegalArgumentException("Invalid index");
+        }
+        if(index == values.length) return max;
+        if(values[index] > max) max = values[index];
+        return max(values, index + 1, max);
+    }
+
+    public void validateArrayIndexes(int start, int end){
+        if(start < 0 || start >= values.length ||
+            end < 0 || end >= values.length
+        ){
+            throw new IllegalArgumentException("Invalid index");
+        }
     }
 
 }
