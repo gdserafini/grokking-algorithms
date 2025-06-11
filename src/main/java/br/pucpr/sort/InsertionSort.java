c801d05e1e84e7dec642d62cee349c1e1804a5f1
Giordano D. Serafini <gdserafini@outlook.com>
2025-06-11 17:00:04 -0300

    Create InsertionSort.java

---
+++ src/main/java/br/pucpr/sort/InsertionSort.java
@@ -0,0 +1,22 @@
+package br.pucpr.sort;
+
+public class InsertionSort<T extends Comparable<T>> {
+
+    public T[] sort(T[] values) {
+        if (values == null || values.length == 0) return values;
+
+        for (int i = 1; i < values.length; i++) {
+            T current = values[i];
+            int j = i - 1;
+
+            while (j >= 0 && values[j].compareTo(current) > 0) {
+                values[j + 1] = values[j];
+                j--;
+            }
+
+            values[j + 1] = current;
+        }
+
+        return values;
+    }
+}
