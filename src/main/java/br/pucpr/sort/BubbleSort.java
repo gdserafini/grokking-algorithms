651f361c722fb6410d09ac6abd051177eecd52ff
Giordano D. Serafini <gdserafini@outlook.com>
2025-06-11 16:56:32 -0300

    Update BubbleSort.java

--- src/main/java/br/pucpr/sort/BubbleSort.java
+++ src/main/java/br/pucpr/sort/BubbleSort.java
@@ -4,15 +4,19 @@ public class BubbleSort<T extends Comparable<T>> {
 
     public T[] sort(T[] values){
         if(values == null || values.length == 0) return values;
+
+        int subArraySizeDecrease = 0;
         
-        for(int i = 0; i < values.length; i++){
-            for(int j = 0; j < values.length; j++){
-                if(values[i].compareTo(values[j]) < 0){
-                    T temp = values[i];
-                    values[i] = values[j];
-                    values[j] = temp;
+        for(int i = 0; i < values.length; i++){            
+            for(int j = 0; j < values.length - 1 - subArraySizeDecrease; j++){
+                if(values[i].compareTo(values[j + 1]) > 0){
+                    T temp = values[j];
+                    values[j] = values[j + 1];
+                    values[j + 1] = temp;
                 }
             }
+
+            subArraySizeDecrease++;
         }
         return values;
     }
