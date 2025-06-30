021fe5ed90e2eca33e5208f384e78ef1ac04cf03
Giordano D. Serafini <gdserafini@outlook.com>
2025-06-30 15:23:25 -0300

    Update InsertionSortTest.java

--- src/test/java/br/pucpr/sort/InsertionSortTest.java
+++ src/test/java/br/pucpr/sort/InsertionSortTest.java
@@ -6,32 +6,11 @@ import org.junit.jupiter.api.Test;
 public class InsertionSortTest {
 
     @Test
-    public void testInsertionSortUnsorted(){
+    public void testInsertionSort(){
         Integer[] numbers = {-1,-6,4,8,3,8};
         Integer[] sortedNumbers = {-6,-1,3,4,8,8};
         var insertionSort = new InsertionSort<Integer>();
         assertArrayEquals(sortedNumbers, insertionSort.sort(numbers));
     }
 
-    @Test
-    public void testInsertionSortSorted(){
-        Integer[] sortedNumbers = {-1,-2,3,4,5};
-        var insertionSort = new InsertionSort<Integer>();
-        assertArrayEquals(sortedNumbers, insertionSort.sort(sortedNumbers));
-    }
-
-    @Test
-    public void testInsertionSortReversed(){
-        Integer[] sortedNumbers = {-2,-1,3,4,5};
-        Integer[] reversedNumbers = {5,4,3,-1,-2};
-        var insertionSort = new InsertionSort<Integer>();
-        assertArrayEquals(sortedNumbers, insertionSort.sort(reversedNumbers));
-    }
-
-    @Test
-    public void testInsertionSortEmpty(){
-        Integer[] emptyNumbers = {};
-        var insertionSort = new InsertionSort<Integer>();
-        assertArrayEquals(emptyNumbers, insertionSort.sort(emptyNumbers));
-    }
 }
