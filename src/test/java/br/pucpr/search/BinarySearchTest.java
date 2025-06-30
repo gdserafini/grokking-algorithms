d6d6d55b091b4632a35903d8081aa0b1c4dcdfcb
Giordano D. Serafini <gdserafini@outlook.com>
2025-06-30 15:22:22 -0300

    Update BinarySearchTest.java

--- src/test/java/br/pucpr/search/BinarySearchTest.java
+++ src/test/java/br/pucpr/search/BinarySearchTest.java
@@ -4,35 +4,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BinarySearchTest {
-    private Integer[] numbers = {1,2,3,4,5,6,7,8,9};
-
+    
     @Test
     public void testBinarySearchFindMid(){
-        assertEquals(BinarySearch.search(numbers, 5), 4);
-    }
-
-    @Test
-    public void testBinarySearchFindLow(){
-        assertEquals(BinarySearch.search(numbers, 3), 2);
-    }
-
-    @Test
-    public void testBinarySearchFindHigh(){
+        private Integer[] numbers = {1,2,3,4,5,6,7,8,9};
         assertEquals(BinarySearch.search(numbers, 7), 6);
-    }
-
-    @Test
-    public void testBinarySearchNotFind(){
-        assertEquals(BinarySearch.search(numbers, 10), -1);
-    }
-
-    @Test
-    public void testBinarySearchStartOfList(){
-        assertEquals(BinarySearch.search(numbers, 9), 8);
-    }
-
-    @Test
-    public void testBinarySearchEndOfList(){
-        assertEquals(BinarySearch.search(numbers, 1), 0);
     }
 }
