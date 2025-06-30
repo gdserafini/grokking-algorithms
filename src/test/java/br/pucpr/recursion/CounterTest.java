8d86b809ef015b1850547289cfe0b2a9ec3d8bad
Giordano D. Serafini <gdserafini@outlook.com>
2025-06-30 15:18:19 -0300

    Update CounterTest.java

--- src/test/java/br/pucpr/recursion/CounterTest.java
+++ src/test/java/br/pucpr/recursion/CounterTest.java
@@ -1,12 +1,13 @@
 package br.pucpr.recursion;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CounterTest {
 
     @Test
     public void testCounter(){
         var counter = new Counter();
-        counter.count(10);
+        assertEquals(counter.count(10), 0);
     }
 }
