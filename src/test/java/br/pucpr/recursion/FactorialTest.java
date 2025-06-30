994ae1582f8811d768f05896f21e69c8b9f14942
Giordano D. Serafini <gdserafini@outlook.com>
2025-06-30 15:19:31 -0300

    Update FactorialTest.java

--- src/test/java/br/pucpr/recursion/FactorialTest.java
+++ src/test/java/br/pucpr/recursion/FactorialTest.java
@@ -1,49 +1,13 @@
 package br.pucpr.recursion;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FactorialTest {
 
     @Test
-    public void testFactorialOfZero() {
-        Factorial factorial = new Factorial();
-        assertEquals(1, factorial.calc(0), "0! should be 1");
-    }
-
-    @Test
-    public void testFactorialOfPositiveInteger() {
-        Factorial factorial = new Factorial();
+    public void testFactorial() {
+        var factorial = new Factorial();
         assertEquals(120, factorial.calc(5), "5! should be 120");
-    }
-
-    @Test
-    public void testFactorialOfNegativeIntegerThrowsException() {
-        Factorial factorial = new Factorial();
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            factorial.calc(-3);
-        });
-        assertEquals("Invalid value: -3", exception.getMessage());
-    }
-
-    @Test
-    public void testFactorialTailOfZero() {
-        Factorial factorial = new Factorial();
-        assertEquals(1, factorial.calcTail(0), "0! should be 1");
-    }
-
-    @Test
-    public void testFactorialTailOfPositiveInteger() {
-        Factorial factorial = new Factorial();
-        assertEquals(120, factorial.calcTail(5), "5! should be 120");
-    }
-
-    @Test
-    public void testFactorialTailOfNegativeIntegerThrowsException() {
-        Factorial factorial = new Factorial();
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            factorial.calcTail(-3);
-        });
-        assertEquals("Invalid value: -3", exception.getMessage());
     }
 }
