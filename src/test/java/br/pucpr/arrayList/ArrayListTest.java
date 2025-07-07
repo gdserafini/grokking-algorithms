59b0bcf9b2369e01d1595ea80d7a17e6ca1da52c
Giordano D. Serafini <gdserafini@outlook.com>
2025-07-07 14:35:37 -0300

    Update ArrayListTest.java

--- src/test/java/br/pucpr/arrayList/ArrayListTest.java
+++ src/test/java/br/pucpr/arrayList/ArrayListTest.java
@@ -6,6 +6,17 @@ import org.junit.jupiter.api.Test;
 public class ArrayListTest {
 
     @Test
-    public void test(){}
+    public void testAdd(){
+        var list = new ArrayList<Integer>();
+        list.add(1);
+        assertEquals(1, list.get());
+    }
 
+    @Test
+    public void testAddAtIndex(){
+        var list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2, 0);
+        assertEquals(2, list.get(0));
+    }
 }
