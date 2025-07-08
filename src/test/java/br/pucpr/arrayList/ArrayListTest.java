5d121c18854bb5f6980d2cc219fa9425bc81766e
Giordano D. Serafini <gdserafini@outlook.com>
2025-07-08 13:15:44 -0300

    Update ArrayListTest.java

--- src/test/java/br/pucpr/arrayList/ArrayListTest.java
+++ src/test/java/br/pucpr/arrayList/ArrayListTest.java
@@ -19,4 +19,39 @@ public class ArrayListTest {
         list.add(2, 0);
         assertEquals(2, list.get(0));
     }
+
+    @Test
+    public void testGetByIndex(){
+        var list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        assertEquals(2, list.get(1));
+    }
+
+    @Test
+    public void testRemoveByIndex(){
+        var list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.remove(1);
+        assertEquals(null, list.get(1));
+    }
+
+    @Test
+    public void testRemove(){
+        var list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.remove();
+        assertEquals(null, list.get(1));
+    }
+
+    @Test
+    public void testSetByIndex(){
+        var list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.set(3, 1);
+        assertEquals(3, list.get(1));
+    }
 }
