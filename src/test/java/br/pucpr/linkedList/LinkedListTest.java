ccc435ebf5e4d645dbd6958620a69d2ac392cd16
Giordano D. Serafini <gdserafini@outlook.com>
2025-07-10 12:57:26 -0300

    Update LinkedListTest.java

--- src/test/java/br/pucpr/linkedList/LinkedListTest.java
+++ src/test/java/br/pucpr/linkedList/LinkedListTest.java
@@ -10,10 +10,27 @@ public class LinkedListTest {
         var numbers = new LinkedList<Integer>();
         numbers.add(1);
         numbers.add(2);
-        numbers.add(3);
         assertEquals(1, numbers.get(0));
         assertEquals(2, numbers.get(1));
-        assertEquals(3, numbers.get(2));
+    }
+
+    @Test
+    public void testAddAtIndex(){
+        var numbers = new LinkedList<Integer>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3, 1);
+        assertEquals(1, numbers.get(0));
+        assertEquals(3, numbers.get(1));
+    }
+
+    @Test
+    public void testAddWithKey(){
+        var numbers = new LinkedList<Integer>();
+        numbers.add("One", 1);
+        numbers.add("Two", 2);
+        assertEquals(1, numbers.getByKey("One"));
+        assertEquals(2, numbers.getByKey("Two"));
     }
 
     @Test
@@ -23,15 +40,6 @@ public class LinkedListTest {
         numbers.add(2);
         numbers.add(3);
         assertEquals(numbers.search(1).getData(), 2);
-    }
-
-    @Test
-    public void testGetLinkedList(){
-        var numbers = new LinkedList<Integer>();
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(3);
-        assertEquals(numbers.get(1), 2);
     }
 
     @Test
@@ -50,5 +58,50 @@ public class LinkedListTest {
         numbers.add(2);
         numbers.add(3);
         assertEquals(numbers.getIndex(2), 1);
+    }
+
+    @Test
+    public void testUpdate(){
+        var numbers = new LinkedList<Integer>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.update(3, 1)
+        assertEquals(numbers.get(1), 3);
+    }
+
+    @Test
+    public void testUpdateByData(){
+        var numbers = new LinkedList<Integer>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.updateByData(3, 2)
+        assertEquals(numbers.get(1), 3);
+    }
+
+    @Test
+    public void testDeleteLast(){
+        var numbers = new LinkedList<Integer>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.delete();
+        assertEquals(numbers.get(1), null);
+    }
+
+    @Test
+    public void testDeleteByIndex(){
+        var numbers = new LinkedList<Integer>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.delete(1);
+        assertEquals(numbers.get(1), null);
+    }
+
+    @Test
+    public void testDeleteByData(){
+        var numbers = new LinkedList<Integer>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.deleteByData(2);
+        assertEquals(numbers.get(1), null);
     }
 }
