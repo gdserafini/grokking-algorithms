cdc2861948bcbc96680aca9d7aed9dd6e1616aec
Giordano D. Serafini <gdserafini@outlook.com>
2025-07-22 14:43:01 -0300

    Update Graph.java

--- src/main/java/br/pucpr/graph/Graph.java
+++ src/main/java/br/pucpr/graph/Graph.java
@@ -1,5 +1,13 @@
 package br.pucpr.graph;
 
-public class Graph {
+public class Graph implements <Comparable <T>>{
+    private HashTable<Queue<T>> graph;
 
+    public Graph(){
+        this.graph = new HashTable<Queue<T>>;
+    }
+}
+
+private class Queue extends LinkedList{
+    
 }
